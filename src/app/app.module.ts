3bbfcf8f8ab09424dbb388deaef18ce1dce016f9
Sushmitha Rasmani <sushmitharasmani2700@gmail.com>
2020-06-25 16:18:13 +0530

    basic service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,7 +12,7 @@ import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
-// import { Ma}
+import { DishService } from './services/dish.service';
 @NgModule({
   declarations: [
     AppComponent,
@@ -30,7 +30,7 @@ import { DishdetailComponent } from './dishdetail/dishdetail.component';
     MatButtonModule,
     FlexLayoutModule
   ],
-  providers: [],
+  providers: [DishService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Dish } from '../shared/dish';
 import { DISHES } from '../shared/dishes';
+import { DishService } from '../services/dish.service';
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
@@ -8,11 +9,11 @@ import { DISHES } from '../shared/dishes';
 })
 export class MenuComponent implements OnInit {
 
-    dishes: Dish[] = DISHES;
+    dishes: Dish[];
     selectedDish: Dish;
-    constructor() { 
-    }
+    constructor(private dishService: DishService) { }
   ngOnInit() {
+    this.dishes=this.dishService.getDishes();
   }
   onSelect(dish: Dish) {
     this.selectedDish = dish;
