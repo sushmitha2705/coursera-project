22f568b07fbfb59a88c84eb9163a27b8e4f8a723
Sushmitha Rasmani <sushmitharasmani2700@gmail.com>
2020-07-09 14:46:58 +0530

    dialog

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,6 +20,8 @@ import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
 import { AppRoutingModule } from './app-routing/app-routing.module';
 import { LeaderService } from './services/leader.service';
+import { LoginComponent } from './login/login.component';
+import { MatDialogModule } from '@angular/material/dialog';
 @NgModule({
   declarations: [
     AppComponent,
@@ -29,7 +31,8 @@ import { LeaderService } from './services/leader.service';
     FooterComponent,
     AboutComponent,
     HomeComponent,
-    ContactComponent
+    ContactComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
@@ -41,12 +44,16 @@ import { LeaderService } from './services/leader.service';
     MatCardModule,
     MatButtonModule,
     FlexLayoutModule,
+    MatDialogModule,
     AppRoutingModule
   ],
   providers: [
     DishService,
     LeaderService
   ],
+  entryComponents: [
+    LoginComponent
+],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
