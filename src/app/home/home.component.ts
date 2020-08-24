b747afe7ca76924c830bdde80046c1bf2865a287
Sushmitha Rasmani <sushmitharasmani2700@gmail.com>
2020-08-24 20:19:23 +0530

    task-2

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -27,13 +27,15 @@ export class HomeComponent implements OnInit {
   promotion: Promotion;
   leader: Leader;
   leaderErrMess : string;
+  promotionErrMess : string;
 
   constructor(private dishservice: DishService,
     private promotionservice: PromotionService,private leaderservice: LeaderService, @Inject('baseURL') private baseURL) { }
 
   ngOnInit() {
     this.dishservice.getFeaturedDish().subscribe(dish => this.dish=dish,errmess => this.dishErrMess = <any>errmess);
-     this.promotionservice.getFeaturedPromotion().subscribe(promotion => this.promotion=promotion);
+    // this.promotionservice.getFeaturedPromotion().subscribe(promotion => this.promotion=promotion);
+     this.promotionservice.getFeaturedPromotion().subscribe(promotion => this.promotion=promotion,errmess => this.promotionErrMess = <any>errmess);
     //this.leaderservice.getFeaturedLeader().subscribe(leader => this.leader=leader);
     this.leaderservice.getFeaturedLeader().subscribe(leader => this.leader=leader,errmess => this.leaderErrMess = <any>errmess);
     
